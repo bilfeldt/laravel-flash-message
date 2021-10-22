2b3f15129e04c32fcdcb3f3ed4a9a12023d1e119
Anders Bilfeldt <abilfeldt@gmail.com>
2021-10-22 14:50:54 +0200

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Bilfeldt\LaravelFlashMessage\Tests;
 
 use Bilfeldt\LaravelFlashMessage\FlashMessageServiceProvider;
+use Faker\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
