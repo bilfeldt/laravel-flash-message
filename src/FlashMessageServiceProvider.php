ba4be2f7b26cb67b7303f03ea89ac0bcfae5696f
Anders Bilfeldt <abilfeldt@gmail.com>
2021-11-11 23:44:07 +0100

    Create new method 'withGlobalErrors'

--- src/FlashMessageServiceProvider.php
+++ src/FlashMessageServiceProvider.php
@@ -4,6 +4,7 @@ namespace Bilfeldt\LaravelFlashMessage;
 
 use Bilfeldt\LaravelFlashMessage\View\Components\Alert;
 use Bilfeldt\LaravelFlashMessage\View\Components\AlertMessages;
+use Illuminate\Support\ViewErrorBag;
 use Illuminate\View\Factory;
 use Illuminate\View\View;
 use Spatie\LaravelPackageTools\Package;
@@ -80,5 +81,36 @@ class FlashMessageServiceProvider extends PackageServiceProvider
 
             return $this;
         });
+
+        /**
+         * Flash a container of errors to the global errors bag.
+         *
+         * Note that the view already has a method 'withErrors' but that does not share them globally like this method.
+         *
+         * @param  \Illuminate\Contracts\Support\MessageProvider|array|string  $provider
+         * @param  string  $key
+         * @return $this
+         * @see https://github.com/laravel/framework/pull/39459
+         */
+        View::macro('withGlobalErrors', function ($provider, $key = 'default'): View {
+            /** @var ViewFlashMessageBag $viewFlashMessageBag */
+            $viewErrorBag = \Illuminate\Support\Facades\View::shared('errors', new ViewErrorBag());
+
+            if ($viewErrorBag->hasBag($key)) {
+                $viewErrorBag->getBag($key)->merge($provider);
+            } else {
+                $viewErrorBag->put($key, $this->formatErrors($provider));
+            }
+
+            \Illuminate\Support\Facades\View::share('errors', $viewFlashMessageBag);
+
+            return $this;
+        });
+
+        // This will ensure that the $message variable is always available even if the ShareMessagesFromSession middleware is not applied.
+        //\Illuminate\Support\Facades\View::share(
+        //    config('flash-message.view_share'),
+        //    \Illuminate\Support\Facades\View::shared(config('flash-message.view_share'), new ViewFlashMessageBag())
+        //);
     }
 }
