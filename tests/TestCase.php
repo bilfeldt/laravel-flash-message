b2491ee0cad9ecca0e8f67dca3e33ceea8cc7841
Bilfeldt <abilfeldt@gmail.com>
2022-02-20 21:49:23 +0100

    StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Bilfeldt\LaravelFlashMessage\Tests;
 
 use Bilfeldt\LaravelFlashMessage\FlashMessageServiceProvider;
-use Faker\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
