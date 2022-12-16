a232d698942e96ed73a320e8c4934ab6b0961262
Anders Bilfeldt <abilfeldt@gmail.com>
2022-12-16 10:03:17 +0100

    Add tests

---
+++ tests/AlertMessageTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Bilfeldt\LaravelFlashMessage\Tests;
+
+use Bilfeldt\LaravelFlashMessage\View\Components\AlertMessage;
+
+class AlertMessageTest extends \Bilfeldt\LaravelFlashMessage\Tests\TestCase
+{
+    public function test_accessibility(): void
+    {
+        $view = $this->component(AlertMessage::class, [
+            'text' => 'Example text',
+        ]);
+
+        $view->assertSee('role="alert"', false);
+    }
+
+    public function test_component_with_only_text(): void
+    {
+        $view = $this->component(AlertMessage::class, [
+            'text' => 'Example text',
+        ]);
+
+        $view->assertSee('Example text');
+    }
+
+    public function test_component_with_title(): void
+    {
+        $view = $this->component(AlertMessage::class, [
+            'text' => 'Example text',
+            'title' => 'Example Title',
+        ]);
+
+        $view
+            ->assertSee('Example text')
+            ->assertSee('Example Title');
+    }
+
+    public function test_component_with_messages(): void
+    {
+        $view = $this->component(AlertMessage::class, [
+            'text' => 'Example text',
+            'messages' => [
+                'field_1' => 'Example message 1',
+                'field_2' => 'Example message 2',
+            ],
+        ]);
+
+        $view
+            ->assertSee('Example text')
+            ->assertSee('Example message 1')
+            ->assertSee('Example message 2');
+    }
+
+    public function test_component_with_links(): void
+    {
+        $view = $this->component(AlertMessage::class, [
+            'text' => 'Example text',
+            'links' => [
+                'Link 1' => 'https://example.com/test1',
+                'Link 2' => 'https://example.com/test2',
+            ],
+        ]);
+
+        $view
+            ->assertSee('Example text')
+            ->assertSee('Link 1')
+            ->assertSee('https://example.com/test1')
+            ->assertSee('Link 2')
+            ->assertSee('https://example.com/test2');
+    }
+
+    public function test_can_render_with_only_text(): void
+    {
+        $view = $this->blade(
+            '<x-flash-alert-message :text="$text" />',
+            ['text' => 'Example text']
+        );
+
+        $view
+            ->assertSee('role="alert"', false)
+            ->assertSee('Example text');
+    }
+
+    public function test_can_render_with_title(): void
+    {
+        $view = $this->blade('<x-flash-alert-message :text="$text" :title="$title" />', [
+            'text' => 'Example text',
+            'title' => 'Example Title',
+        ]);
+
+        $view
+            ->assertSee('role="alert"', false)
+            ->assertSee('Example text')
+            ->assertSee('Example Title');
+    }
+
+    public function test_can_render_with_messages(): void
+    {
+        $view = $this->blade('<x-flash-alert-message :text="$text" :messages="$messages" />', [
+            'text' => 'Example text',
+            'messages' => [
+                'field_1' => 'Example message 1',
+                'field_2' => 'Example message 2',
+            ],
+        ]);
+
+        $view
+            ->assertSee('role="alert"', false)
+            ->assertSee('Example text')
+            ->assertSee('Example message 1')
+            ->assertSee('Example message 2');
+    }
+
+    public function test_can_render_with_links(): void
+    {
+        $view = $this->blade('<x-flash-alert-message :text="$text" :links="$links" />', [
+            'text' => 'Example text',
+            'links' => [
+                'Link 1' => 'https://example.com/test1',
+                'Link 2' => 'https://example.com/test2',
+            ],
+        ]);
+
+        $view
+            ->assertSee('role="alert"', false)
+            ->assertSee('Example text')
+            ->assertSee('Link 1')
+            ->assertSee('https://example.com/test1')
+            ->assertSee('Link 2')
+            ->assertSee('https://example.com/test2');
+    }
+}
