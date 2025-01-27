88ab0ef1ff3765bf1f47b5462dd26b84158e5ff0
Bilfeldt <abilfeldt@gmail.com>
2025-01-27 14:57:54 +0100

    Fix tests

--- tests/AlertTest.php
+++ tests/AlertTest.php
@@ -4,10 +4,12 @@ namespace Bilfeldt\LaravelFlashMessage\Tests;
 
 use Bilfeldt\LaravelFlashMessage\Message;
 use Bilfeldt\LaravelFlashMessage\View\Components\Alert;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 
 class AlertTest extends \Bilfeldt\LaravelFlashMessage\Tests\TestCase
 {
-    public function provideLevels()
+    public static function provideLevels()
     {
         return [
             [Message::LEVEL_MESSAGE],
@@ -18,9 +20,8 @@ class AlertTest extends \Bilfeldt\LaravelFlashMessage\Tests\TestCase
         ];
     }
 
-    /**
-     * @dataProvider provideLevels
-     */
+    #[Test]
+    #[DataProvider('provideLevels')]
     public function test_can_show_message(string $level): void
     {
         $view = $this->component(Alert::class, [
@@ -31,9 +32,8 @@ class AlertTest extends \Bilfeldt\LaravelFlashMessage\Tests\TestCase
         $view->assertSee('role="alert"', false);
     }
 
-    /**
-     * @dataProvider provideLevels
-     */
+    #[Test]
+    #[DataProvider('provideLevels')]
     public function test_can_render_message(string $level): void
     {
         $view = $this->blade('<x-flash::alert :level="$level" :text="$text" />', [
